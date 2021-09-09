3f69ccbcb0632fe64c00bf7ec23c0701b6617c36
Jaime Aricapa <2241@holbertonschool.com>
2021-09-09 11:46:32 -0500

    update

--- Platzi_exercises/Curso_Django/users/admin.py
+++ Platzi_exercises/Curso_Django/users/admin.py
@@ -1,4 +1,6 @@
 """User admin classes."""
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from django.contrib import admin
 from users.models import Profile
 
@@ -17,3 +19,39 @@ class ProfileAdmin(admin.ModelAdmin):
     search_fields = ('user__email', 'user__username', 'user__first_name', 'user__last_name', 'phone_numer')
     # allow to admin filter fields
     list_filter = ('user__is_active', 'user__is_staff', 'created', 'modified')
+    # set the visual fields for input data when create a new profile
+    fieldsets = (
+        ('Profile',{
+            'fields':(('user','picture'),)
+        }),
+        ('Extra info',{
+            'fields':(
+                ('website','phone_number'),
+                ('biography'),
+            )
+        }),
+        ('Metadata',{
+            'fields':(('created','modified'),),
+        })
+    )
+    readonly_fields = ('created','modified')
+"""
+Une los modelos de usuario y perfil para no tener que crear un usuario para asociarlo con un perfil
+"""
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+class UserAdmin(UserAdmin):
+    inlines = (ProfileInLine,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+admin.site.unregister(User)
+admin.site.register(User,UserAdmin)
