9933148b514e6b15c3e9e8bf8c7bd412ac1ad4b8
Jaime Aricapa <2241@holbertonschool.com>
2021-09-14 11:15:25 -0500

    update files

--- Platzi_exercises/Curso_Django/platzigram/settings.py
+++ Platzi_exercises/Curso_Django/platzigram/settings.py
@@ -142,3 +142,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
--- ejercicios_libres/prueba.py
+++ ejercicios_libres/prueba.py
@@ -1,3 +1,4 @@
+  
 def is_leap(year):
     leap = False
     
