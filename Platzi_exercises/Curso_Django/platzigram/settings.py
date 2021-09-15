5f316b29aa93345210dfd743fb06d32fde648874
Jaime Aricapa <2241@holbertonschool.com>
2021-09-15 06:50:45 -0500

    create login page and redirections

--- Platzi_exercises/Curso_Django/platzigram/settings.py
+++ Platzi_exercises/Curso_Django/platzigram/settings.py
@@ -143,4 +143,6 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'accounts.User'
+#AUTH_USER_MODEL = 'accounts.User'
+
+LOGIN_URL = '/users/login/'
--- Platzi_exercises/Curso_Django/platzigram/urls.py
+++ Platzi_exercises/Curso_Django/platzigram/urls.py
@@ -1,23 +1,21 @@
 """Platzigram URLs module."""
 
-# Django
+# Django apps
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-
+# Created apps
 from platzigram import views as local_views
 from posts import views as posts_views
+from users import views as users_views
 
 
 urlpatterns = [
-
     path('admin/', admin.site.urls),
-
-    path('hello-world/', local_views.hello_world),
-    path('sorted/', local_views.sorted),
-    path('hi/<str:name>/<int:age>/', local_views.say_hi),
-
-    path('posts/', posts_views.list_posts),
-
+    path('hello-world/', local_views.hello_world, name='hello-world'),
+    path('sorted/', local_views.sorted, name='sorted'),
+    path('hi/<str:name>/<int:age>/', local_views.say_hi, name='say_hi'),
+    path('posts/', posts_views.list_posts, name='feed'),
+    path('users/login/', users_views.login_view, name='login')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
