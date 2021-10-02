0a00e68ecb7ac4e34ec8c3b3777834998984e3bc
Jaime Aricapa <2241@holbertonschool.com>
2021-10-02 17:09:24 -0500

    create Generic auth views

--- Platzi_exercises/Curso_Django/platzigram/settings.py
+++ Platzi_exercises/Curso_Django/platzigram/settings.py
@@ -82,13 +82,9 @@ WSGI_APPLICATION = 'platzigram.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "abbahijo12345",
-        "HOST": "localhost",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -143,3 +139,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 LOGIN_URL = '/users/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = LOGIN_URL
