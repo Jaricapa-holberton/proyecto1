3cbe3049c2b81ef497b62dbd422f25f15cfbff4b
Jaime Aricapa <2241@holbertonschool.com>
2021-12-19 19:40:11 -0500

    add final validations

--- fast-api-hello-world/main1.py
+++ fast-api-hello-world/main1.py
@@ -1,102 +1,135 @@
-#Python
 from typing import Optional
 from enum import Enum
 
-#Pydantic
+# Pydantic
 from pydantic import BaseModel, Field
 
-#FastAPI
+# FastAPI
 from fastapi import FastAPI, Body, Query, Path
 
 app = FastAPI()
 
-#Models
+class HairColor(Enum):
+    white = "white"
+    brown = "brown"
+    black = "black"
+    blonde = "blonde"
+    red = "red"
+
 
 class Location(BaseModel):
-    city: str 
-    state: str
-    country: str
-    
-class HairColor(Enum):
-    white = 'white'
-    black = 'black'
-    brown = 'brown'
-    blonde = 'blonde'
+    city: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        description="The city where the person lives",
+        example="New York",
+    )
+    state: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        description="The state where the person lives",
+        example="New York",
+    )
+    country: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        description="The country where the person lives",
+        example="United States",
+    )
 
 class Person(BaseModel):
     first_name: str = Field(
         ...,
         min_length=1,
-        max_length=50
-    )
+        max_length=50,
+        example="Miguel"
+        )
     last_name: str = Field(
         ...,
         min_length=1,
-        max_length=50
-    )
+        max_length=50,
+        example="Gonzalez"
+        )
     age: int = Field(
         ...,
         gt=0,
-        le=130
+        le=115,
+        example=25
     )
-    hair_color: Optional[HairColor] = Field(default=None)
-    is_married: Optional[bool] = Field(default=None)  
+    hair_color: Optional[HairColor  ] = Field(default=None, example=HairColor.brown)
+    is_married: Optional[bool] = Field(default=None, example=False)
 
-@app.get("/")                         #Un path operation que usa el método get en la ruta /
+    class Config:
+       schema_extra = {
+           "example": {
+               "first_name": "Facundo",
+               "last_name": "García Martoni",
+               "age": 29,
+               "hair_color": "blonde",
+               "is_married": False,
+           }
+       }
+
+@app.get("/")
 def home():
-    return {"Hello": "World"}
+    return {"Hello": "World!"}
 
-@app.post("/person/new")              #Un path operation que usa el metodo post en la ruta /person/new
-def create_person(person: Person = Body(...)):  #Se usa un Request Body, se importa la clase Body de FastAPI esta en su constructor usa un parametro, en este caso son                                                    los 3 puntos, que indican que ese Request Body es obligatorio. Se usa una variable que serviría como parametro para                                                      usar el modelo, en este caso person que sería tipo Person
+# Request and Response Body
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
     return person
 
-# Validations: Query Parameters
+# Validaciones: Query Parameters
 
-@app.get('/person/detail')
+@app.get("/person/detail")
 def show_person(
     name: Optional[str] = Query(
-        None,
-        min_length=1,
-        max_length=30,
-        regex='^[A-Za-z]*$',
+        None, min_length=1,
+        max_length=50,
         title="Person Name",
-        description="This is the person name. It's between 1 and 30 characters"
-    ),
-    age: int = Query(
+        description="This is the person name,  It's between 1 and 50 characters",
+        example="Rocio"
+        ),
+    age: str = Query(
         ...,
         title="Person Age",
-        description="This is the person age. It's required"
-    )
+        description="This is the person age, It's required",
+        example="25"
+        )
 ):
     return {name: age}
 
+# Validaciones Path Paremeters
 
-# Validations: Path Parameters
-
-@app.get('/person/detail/{person_id}')
+@app.get("/person/detail/{person_id}")
 def show_person(
     person_id: int = Path(
         ...,
-        title="Person ID",
-        description="This is the person ID. It's required",
-        gt=0
+        gt=0,
+        example=123
     )
 ):
     return {person_id: "It exists!"}
 
-# Validations: Request Body
+# Validaciones: Request Body
+
 @app.put("/person/{person_id}")
 def update_person(
     person_id: int = Path(
         ...,
         title="Person ID",
         description="This is the person ID",
-        gt=0
+        gt=0,
+        example=123
     ),
     person: Person = Body(...),
-    location: Location = Body(...)
+    #location: Location = Body(...)
 ):
-    result = dict(person)
-    result.update(dict(location))
-
-    return result
+    #results = person.dict()
+    #results.update(location.dict())
+    #return results
+    return person
